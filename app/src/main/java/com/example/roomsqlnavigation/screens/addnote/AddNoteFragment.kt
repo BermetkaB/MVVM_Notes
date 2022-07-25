89b2eb92d50e9db389311091e12dd558b0ab8dc6
Bermet <bermet.bainaeva.ch@gmail.com>
2022-07-25 20:42:00 +0600

    mistakes again

--- app/src/main/java/com/example/roomsqlnavigation/screens/addnote/AddNoteFragment.kt
+++ app/src/main/java/com/example/roomsqlnavigation/screens/addnote/AddNoteFragment.kt
@@ -5,17 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.roomsqlnavigation.APP
 import com.example.roomsqlnavigation.R
+import com.example.roomsqlnavigation.databinding.FragmentAddNoteBinding
 
 
 class AddNoteFragment : Fragment() {
+    lateinit var binding: FragmentAddNoteBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_note, container, false)
+
+        binding= FragmentAddNoteBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        unitAdd()
+    }
+
+
+    private fun unitAdd(){
+        val viewModel = viewModelProvider(this).get(AddNoteViewModel::class.java)
+        binding.btnDetailBack.setOnClickListener {
+            viewModel.insert()
+        }
+        binding.btnDetailBack.setOnClickListener {
+            APP.navController.navigate(R.id.action_addNoteFragment_to_startFragment)
+        }
+    }
 }
--- app/src/main/java/com/example/roomsqlnavigation/screens/addnote/AddNoteViewModel.kt
+++ app/src/main/java/com/example/roomsqlnavigation/screens/addnote/AddNoteViewModel.kt
@@ -1,6 +1,16 @@
 package com.example.roomsqlnavigation.screens.addnote
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomsqlnavigation.REPOSITORY
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class AddNoteViewModel:ViewModel() {
+    fun insert(noteModel:NoteModel,onSuccess:() ->Unit)=
+        viewModelScope.launch(Dispatchers.IO){
+            REPOSITORY.insertNote(noteModel){
+                onSuccess()
+            }
+        }
 }
